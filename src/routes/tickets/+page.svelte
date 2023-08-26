38730d92dba5adcb87f2bb1a4ba13cbdf42ed8c8
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2023-08-26 21:37:38 +0200

    Prepare Tickets page

---
+++ src/routes/tickets/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import TipTap from "$lib/TipTap.svelte";
+  import Breadcrumbs from "$lib/Breadcrumbs.svelte";
+</script>
+
+<Breadcrumbs path={[{text: "Home", path: "/"}, {text: "Tickets"}]} />
+
+<h1 class="text-3xl font-bold text-slate-700">Tickets</h1>
