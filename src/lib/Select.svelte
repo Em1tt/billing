aef270b774d95b04e3b08671951fca4e91096408
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2023-09-05 22:50:36 +0200

    Customization on Select component

--- src/lib/Select.svelte
+++ src/lib/Select.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-    export let selected: string = "";
+    let selected: string | null | undefined;
     export let defaultText: string;
-    function onChange(event: any) {
-		selected = event.currentTarget.value;
-        console.log(selected);
-	}
+    export let options: Array<{value: string, text: string, id: string}>
+    export let value: string | null | undefined;
+    let open: boolean = false;
+    function toggleSelect(){
+        open = !open;
+    }
+    function changeText(event: any){
+        selected = ((event.currentTarget as HTMLElement).parentElement)?.getAttribute("data-text");
+        value = ((event.currentTarget as HTMLElement).parentElement)?.parentElement?.querySelector("input")?.value;
+        toggleSelect();
+    }
 </script>
 
 <div class="flex flex-col flex-nowrap w-full relative">
-    <button class="p-2 text-left cursor-pointer bg-transparent w-full border border-slate-400 rounded focus:border-sky-500 outline-none text-slate-600 focus:text-sky-500 peer">{selected == "" ? defaultText : selected}</button>
-    <ul class="border border-slate-400 rounded mt-0.5 peer-focus-within:block absolute top-full w-full bg-slate-200">
+    <button on:click={toggleSelect} class="py-2 px-3 text-left cursor-pointer bg-transparent w-full border border-slate-400/40 rounded focus:border-sky-500 outline-none text-slate-400 focus:text-sky-500 peer">{selected ? selected : defaultText}</button>
+    <ul class="border border-slate-400 z-20 rounded mt-0.5 absolute top-full w-full bg-slate-200 {open ? "block" : "hidden"}">
+        {#each options as option}
         <li class="flex flex-row hover:bg-slate-300 duration-100 w-full group cursor-pointer">
-            <input on:change={onChange} type="radio" id="config" name="category" class="hidden peer" value="1" >
-            <label for="config" class="p-2 peer-checked:text-green-500 cursor-pointer text-slate-600 group-hover:text-slate-800 w-full">Configuration Issues</label>
+            <input type="radio" name="category" class="hidden peer" value={option.value} >
+            <label for="config" class="cursor-pointer text-slate-600 group-hover:text-slate-800 w-full" data-text={option.text}>
+            <button on:click={changeText} class="w-full p-2 text-left">{option.text}</button></label>
         </li>
-        <li class="flex flex-row hover:bg-slate-300 duration-100 w-full group cursor-pointer">
-            <input on:change={onChange} type="radio" id="billing" name="category" class="hidden peer" value="2" >
-            <label for="billing" class="p-2 peer-checked:text-green-500 cursor-pointer text-slate-600 group-hover:text-slate-800 w-full">Billing Issues</label>
-        </li>
+        {/each}
     </ul>
 </div>
