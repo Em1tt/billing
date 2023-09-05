45984715ad006c0df2d2eb0c0a03083b41844316
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2023-09-05 22:52:54 +0200

    Small Cleanup

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -3,7 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export const actions: import('./$types').Actions = {
     default: async () => {
         pb.authStore.clear();
-        console.log(pb.authStore.clear());
         throw redirect(301, "/");
     }
 };
