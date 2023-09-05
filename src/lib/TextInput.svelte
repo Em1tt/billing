9ede616641d9e99ca53932ce6fe8af35836123e0
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2023-09-05 22:50:02 +0200

    Change Input component to TextInput

---
+++ src/lib/TextInput.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let onChange: () => void = () => {};
+    export let value: string = "";
+    export let name: string;
+    export let placeholder: string;
+</script>
+
+<input
+	on:change={onChange}
+	bind:value={value}
+	placeholder=" "
+	type="text"
+	name={name}
+	id={name}
+	class="p-2 bg-transparent w-full border border-slate-400 placeholder-shown:border-slate-400/40 rounded focus:border-sky-500 outline-none text-slate-600 focus:text-sky-500 peer"
+/>
+<label
+	for={name}
+	class="absolute pointer-events-none duration-100 motion-reduce:duration-0 text-slate-400 -top-3 left-1 peer-focus:-top-3 peer-focus:left-1 bg-slate-200 rounded-full px-1 peer-focus:text-sky-500 py-0 h-5 peer-placeholder-shown:top-2 peer-placeholder-shown:text-slate-400 peer-placeholder-shown:left-3"
+	>{placeholder}</label
+>
